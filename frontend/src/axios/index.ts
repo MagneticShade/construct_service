4bb4f70f6e6e7ee79839a9481c44a953dd735b86
eevviirr <hom.1232015@yandex.ru>
2023-09-27 12:30:02 +0200

    new link on server

--- frontend/src/axios/index.ts
+++ frontend/src/axios/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://practice-test.ru:8080/",
 });
 
 export async function getTemplates() {
