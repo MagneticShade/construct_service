d407addd8f33f00a1b2660346ada2b7f2404495e
eevviirr <hom.1232015@yandex.ru>
2023-09-27 13:32:28 +0200

    mask work

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,10 +3,14 @@ import { Menu } from "../components/Menu";
 const Home = () => {
     return (
         <>
-            <div className="container h-screen w-full flex items-center justify-center">
+            <div className="container h-screen w-full flex items-center justify-center bg-white">
                 <div className=" w-full h-screen">
                     <div className="flex items-center justify-center h-screen">
-                        <img src="./mask.png" alt="mask" className="h-[50%]" />
+                        <img
+                            src="/constructorpractice/mask.png"
+                            alt="mask"
+                            className="h-[50%]"
+                        />
                     </div>
                 </div>
                 <Menu userImg="/constructorpractice/user.jpg" />
