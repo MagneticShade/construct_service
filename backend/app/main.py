5021c69c1634c5d46e475526f1d6c47af284c92c
ArtemChuban <ArtemChuban@yandex.ru>
2023-10-02 11:52:41 +0200

    User initial

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pymongo import MongoClient
 
 app = FastAPI()
 
+mongoClient = MongoClient(host="mongo_backend", port=27017)
+database = mongoClient.get_database("backend")
+users_collection = database.get_collection("users")
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+TelegramID = str
+
+
+@app.get("/user/{telegramID}", tags=["User"])
+def get_user(telegramID: TelegramID) -> TelegramID:
+    raise NotImplementedError
+
+
+@app.post("/user/{telegramID}", tags=["User"])
+def post_user(telegramID: TelegramID) -> None:
+    if users_collection.find_one({"telegramID": telegramID}) is not None:
+        raise HTTPException(status_code=400, detail="User exist")
+    users_collection.insert_one({"telegramID": telegramID})
+
+
+@app.put("/user/{telegramID}", tags=["User"])
+def put_user(telegramID: TelegramID):
+    raise NotImplementedError
